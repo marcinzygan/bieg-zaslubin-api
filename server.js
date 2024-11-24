6f5d7fb5fcd112872485e2be0c00854f7aae3d51
marcinzygan <soul@o2.pl>
2024-11-24 16:37:04 +0100

    updated scrape

--- server.js
+++ server.js
@@ -1,8 +1,34 @@
 const mongoose = require("mongoose");
+const { exec } = require("child_process");
+const fs = require("fs");
 const dotenv = require("dotenv");
 //ENV config
 dotenv.config({ path: "./.env.local" });
+function installChromiumDependencies() {
+  return new Promise((resolve, reject) => {
+    exec(
+      "apt-get update && apt-get install -y libnss3 libgdk-pixbuf2.0-0 libx11-xcb1 libxcomposite1 libxrandr2 libatk-bridge2.0-0 libatk1.0-0 libcups2 libnspr4 libxss1",
+      (error, stdout, stderr) => {
+        if (error) {
+          console.error(`Error installing dependencies: ${stderr}`);
+          reject(error);
+        } else {
+          console.log(stdout);
+          resolve();
+        }
+      }
+    );
+  });
+}
 
+installChromiumDependencies()
+  .then(() => {
+    console.log("Chromium dependencies installed successfully!");
+    // Now proceed to run your scraping logic or load your application
+  })
+  .catch((error) => {
+    console.error("Error during installation:", error);
+  });
 // Import the scheduler to run it when the server starts
 // require("./scheduler"); // This line will start the scheduled task
 
